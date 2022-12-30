131b000ab96c5abc25684a0e68fd3869a680b592
sammyshehter <samir_shehter@yahoo.com>
2022-12-30 04:36:47 +0100

    added some missing files

---
+++ client/src/Components/Loader/index.jsx
@@ -0,0 +1,26 @@
+import React from "react"
+import styles from "./loader.module.css"
+
+export const Loader = ({height = "20px", width = "20px"}) => {
+    return (
+        <div
+            className={`${styles.loader}
+        ease-linear
+        rounded-full
+        border-4
+        border-t-4
+        border-gray-200
+        mx-auto
+        `}
+            style={{height, width}}
+        ></div>
+    )
+}
+
+export const FullLoader = () => {
+    return (
+        <div className="fixed top-0 left-0 right-0 bottom-0 w-full h-screen z-50 overflow-hidden bg-green-400 opacity-75 flex flex-col items-center justify-center">
+            <div className={`${styles.loader} ease-linear rounded-full border-4 border-t-4 border-gray-200 h-12 w-12 mb-4`}></div>
+        </div>
+    )
+}
