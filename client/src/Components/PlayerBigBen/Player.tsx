31317d8c83dd918d806ee78c04206256217af113
Yuri <yuri.vynnyk@gmail.com>
2023-02-17 11:46:46 +0200

    fixed herald

--- client/src/Components/PlayerBigBen/Player.tsx
+++ client/src/Components/PlayerBigBen/Player.tsx
@@ -7,16 +7,28 @@ export default function Player ({info, number}: Props.Player){
         }
     }
 
+    const defineHeraldRole = () => {
+        switch (true) {
+            case info.roles.sheriff:
+                return '/Herald-sheriff.jpg'
+        
+            default:
+                return '/Herald.jpg';
+        }
+    }
+
     return (
         <div className="player-wrapper">
             <div className={`player-card citizen ${info.roles.don ? "don" : ""} ${info.roles.mafia ? "mafia" : ""} ${info.roles.sheriff ? "sheriff" : ""} ${info.dead ? "dead" : "alive"} ${info.vote ? "vote" : "noVote"}`}>
                 <div className={styleProps(info.roles)}/>
-                <img src={info.player ? info.player.image : "/Herald.jpg"} alt='#'/>
+                <img src={info.player ? info.player.image : defineHeraldRole()} alt='#'/>
             </div>
+            {info.player &&
             <div className="player-bottom">
-                    <div className={`player-number ${info.roles.don ? "don" : ""} ${info.roles.mafia ? "mafia" : ""} ${info.roles.sheriff ? "sheriff" : ""}`}>{number}</div>
-                    <div className="player-name">{info.player?.name}</div>
-                </div>
+                <div className={`player-number ${info.roles.don ? "don" : ""} ${info.roles.mafia ? "mafia" : ""} ${info.roles.sheriff ? "sheriff" : ""}`}>{number}</div>
+                <div className="player-name">{info.player?.name}</div>
+            </div>
+            }
         </div>
     )
 }
