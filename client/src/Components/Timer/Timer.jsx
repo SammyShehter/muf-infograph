258865d8791d8713788abc2da8f3effd1c226694
sammyshehter <samir_shehter@yahoo.com>
2023-03-03 18:10:45 +0200

    adding timer featrue on main

--- client/src/Components/Timer/Timer.jsx
+++ client/src/Components/Timer/Timer.jsx
@@ -1,87 +1,85 @@
-import React, { useRef, useState } from "react";
-import './Timer.scss'
-
-const timeCounter = (time) => {
-  return time.toString();
-};
+import {useEffect, useRef, useState} from "react"
+import "./Timer.scss"
 
 const TimerGame = () => {
-  const [timeLeft, setTimeLeft] = useState(60);
-  const [isCounting, setIsCounting] = useState(false);
-  const intervalRef = useRef(null);
-  const inputRef = useRef("");
+    const [timeLeft, setTimeLeft] = useState(60)
+    const [overallTime, setOverallTime] = useState(60)
+    const [isCounting, setIsCounting] = useState(false)
+    const intervalRef = useRef(null)
+    const [widthLine, setWidthLine] = useState(
+        (timeLeft * 100) / timeLeft + "%"
+    )
 
-  function startTimer() {
-    if (intervalRef.current !== null) return;
-    setIsCounting(true);
-    inputRef.current = "";
-    intervalRef.current = setInterval(() => {
-      setTimeLeft((timeLeft) => {
-        if (timeLeft >= 1) return timeLeft - 1;
+    useEffect(() => {
+        setWidthLine(calculateLine(overallTime, timeLeft))
+        if (timeLeft === 0) {
+            setIsCounting(false)
+        }
+    }, [timeLeft])
 
-        // reset the timer
+    function startTimer() {
+        if (intervalRef.current) return
+        setIsCounting(true)
+        intervalRef.current = setInterval(() => {
+            setTimeLeft((timeLeft) => {
+                if (timeLeft >= 1) return timeLeft - 1
+                return 0
+            })
+        }, 1000)
+    }
 
-        return 0;
-      });
-    }, 1000);
-  }
+    function stopTimer() {
+        if (!intervalRef.current) return
+        setIsCounting(false)
+        clearInterval(intervalRef.current)
+        intervalRef.current = null
+    }
 
-  function stopTimer() {
-    if (intervalRef.current === null) return;
-    setIsCounting(false);
-    clearInterval(intervalRef.current);
-    intervalRef.current = null;
-  }
+    function resetTimer() {
+        clearInterval(intervalRef.current)
+        setTimeLeft(0)
+        setIsCounting(false)
+        intervalRef.current = null
+        setWidthLine("")
+    }
 
-  function resetTimer() {
-    clearInterval(intervalRef.current);
-    setTimeLeft(0);
-    setIsCounting(false);
-    intervalRef.current = null;
-    setShowLine(false)
-  }
+    function getTime(seconds) {
+        setTimeLeft(seconds)
+        setOverallTime(seconds)
+        setWidthLine(calculateLine(seconds, seconds))
+    }
 
-  function inputTime(e){
-    inputRef.current = e.target.value;
-    setTimeLeft(e.target.value);
-  }
+    function calculateLine(overallSeconds, secondsLeft) {
+        return (secondsLeft * 100) / overallSeconds + "%"
+    }
 
-  const [selectTime, setSelectTime] = useState('');
-  const [showLine, setShowLine] = useState(false);
+    return (
+        <div className="timer-game">
+            <div className="timer">
+                <span>{timeLeft}</span>
+            </div>
+            <div className="player-card">
+                <div
+                    className="timer-line"
+                    style={{
+                        width: widthLine,
+                    }}
+                ></div>
+            </div>
+            <div className="buttons">
+                {isCounting ? (
+                    <button onClick={stopTimer}>Stop</button>
+                ) : (
+                    <button onClick={startTimer}>Start</button>
+                )}
+                <button onClick={resetTimer}>Reset</button>
+            </div>
+            <div className="text">
+                <button onClick={() => getTime(30)}>30</button>
+                <button onClick={() => getTime(60)}>60</button>
+            </div>
+        </div>
+    )
+}
 
-  function getTime (e) {
-    setSelectTime(e.target.value)
-    setTimeLeft(e.target.value);
-    setShowLine(true)
-  }
-
-  const seconds = timeCounter(timeLeft);
-
-  let widthLine = timeLeft*100/selectTime + '%';
-
-
-  return (
-    <div className="timer-game">
-      <div className="timer">
-        <span>{seconds}</span>
-      </div>
-      <div className="player-card">
-        <div className="timer-line" style={{display: `${showLine} ? "block" : "none"`, width: widthLine}}>
-      </div>
-      </div>
-      <div className="buttons">
-        {!isCounting && <button onClick={startTimer}>Start</button>}
-        {isCounting && <button onClick={stopTimer}>Stop</button>}
-        <button onClick={resetTimer}>Reset</button>
-      </div>
-      <div className="text">
-        <label>Enter the time in sec</label>
-        <input type="text" onChange={inputTime} value={inputRef.current} />
-        <button onClick={getTime} value={30}>30</button>
-        <button onClick={getTime} value={60}>60</button>
-      </div>
-    </div>
-  );
-};
-
-export default TimerGame;
+export default TimerGame
